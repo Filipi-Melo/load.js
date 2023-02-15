07ea235978f948bf2870f95e6423be732c1cf4d9
Filipi-Melo <120530188+Filipi-Melo@users.noreply.github.com>
2023-02-15 17:40:32 -0300

    Add files via upload

---
+++ load.js
@@ -0,0 +1,16 @@
+(async function load(){
+    const Load = new FakeReact()
+    Load.start(
+        Load.tag('link',false,{rel:"stylesheet", href:"./load.css"}),
+        Load.tag('span',false,{id:'load'},[
+            Load.tag('div',false,{class:'loading',tabindex:-1},[
+                Load.tag('div',false,{class:'top center float'},[
+                    Load.tag('img',false,{src:'./load.png',class:'img',heigth:"100%",width:"100%"}),
+                    Load.tag('h1','Loading...')
+                ])    
+            ])
+        ])
+    )
+    await new Promise( call => setTimeout(call, Math.random()*1000+300))
+    Load.qs("#load").innerHTML="";
+})()
